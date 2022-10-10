57de4e5589bb10f01b59d24d99e555a27a5da69d
Voltseon <aman.trechsel@student.nhlstenden.com>
2022-10-10 21:35:48 +0200

    Bestelwiel functioneel

--- Speelplaats/script.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    Array.prototype.forEach.call(document.getElementsByClassName("socksSpeelplaatsImg"), (element, index) => {
-        element.style.left = `${25+(index-2)*10}%`;
-    })
-}
