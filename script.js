fb54f5f37c3d0e152585f89eaf7defd8a4b9b271
KaasToast <raeven.van.dijk@student.nhlstenden.com>
2022-10-12 12:01:19 +0200

    finished mobile homepage

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 window.onload = () => {
-    element = document.getElementsByClassName("navDropdown")[0]
-    element.addEventListener("click", () => {
-        if (element.classList.contains("toggled")) {
-            element.classList.remove("toggled");
+    navdropdown = document.getElementsByClassName("navDropdown")[0]
+    headerdropdown = document.getElementsByClassName("headerDropdown")[0]
+    navdropdown.addEventListener("click", () => {
+        if (navdropdown.classList.contains("toggled")) {
+            navdropdown.classList.remove("toggled");
+            headerdropdown.classList.remove("headerOpen")
         } else {
-            element.classList.add("toggled");
+            navdropdown.classList.add("toggled");
+            headerdropdown.classList.add("headerOpen");
         }
+    })
+    footerdropdowns = document.getElementsByClassName("footerItem");
+    footertitles = document.getElementsByClassName("footerItemTitle");
+    Array.prototype.forEach.call(footertitles, (footertitle, index) => {
+        footertitle.addEventListener("click", () => {
+            if (footerdropdowns[index].classList.contains("opened")) {
+                footerdropdowns[index].classList.remove("opened");
+            } else {
+                footerdropdowns[index].classList.add("opened");
+            }
+        })
     })
 }
