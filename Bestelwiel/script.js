310de8864a16bed961ef78e427a932ef51a9d495
AmanTrechsel <aman.trechsel@student.nhlstenden.com>
2022-10-24 10:51:39 +0200

    file cleanup

--- Bestelwiel/script.js
+++ Bestelwiel/script.js
@@ -91,17 +91,17 @@ function getWeetje()
     let weetjeIndex = Math.floor(Math.random() * FEITJES.length);
     if (lastWeetje == "")
     {
-      document.cookie = `last_weetje=${weetjeIndex}`
-      return FEITJES[weetjeIndex];
+        document.cookie = `last_weetje=${weetjeIndex}`
+        return FEITJES[weetjeIndex];
     }
     lastWeetje = parseInt(lastWeetje);
     for (let i = 0; i < FEITJES.length; i++) {
-      weetjeIndex = Math.floor(Math.random() * FEITJES.length);
-      if (weetjeIndex != lastWeetje)
-      {
-          document.cookie = `last_weetje=${weetjeIndex}`
-          return FEITJES[weetjeIndex];
-      }
+        weetjeIndex = Math.floor(Math.random() * FEITJES.length);
+        if (weetjeIndex != lastWeetje)
+        {
+            document.cookie = `last_weetje=${weetjeIndex}`
+            return FEITJES[weetjeIndex];
+        }
     }
     document.cookie = `last_weetje=${weetjeIndex}`
     return FEITJES[weetjeIndex];
@@ -112,13 +112,13 @@ function getCookie(cname) {
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
     for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
 }
