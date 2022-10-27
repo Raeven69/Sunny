c03ccae3b605cadb039722086d5ec264369404ba
KaasToast <112391293+Raeven69@users.noreply.github.com>
2022-10-27 20:57:03 +0200

    fixed refs, carousel, footers + other bugs, normalized code and added comments

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 window.onload = () => {
-    navdropdown = document.getElementsByClassName("navDropdown")[0]
-    headerdropdown = document.getElementsByClassName("headerDropdown")[0]
+    // Variable declaration for the header dropdown
+    navdropdown = document.getElementsByClassName("navDropdown")[0];
+    headerdropdown = document.getElementsByClassName("headerDropdown")[0];
+    // Toggle the dropdown when clicked
     navdropdown.addEventListener("click", () => {
         if (navdropdown.classList.contains("toggled")) {
             navdropdown.classList.remove("toggled");
-            headerdropdown.classList.remove("headerOpen")
+            headerdropdown.classList.remove("headerOpen");
         } else {
             navdropdown.classList.add("toggled");
             headerdropdown.classList.add("headerOpen");
         }
     })
+    // Variable declaration for the dropdowns in the footer
     footerdropdowns = document.getElementsByClassName("footerItem");
     footertitles = document.getElementsByClassName("footerItemTitle");
+    // Add an event listener for all dropdowns to toggle them
     Array.prototype.forEach.call(footertitles, (footertitle, index) => {
         footertitle.addEventListener("click", () => {
             if (footerdropdowns[index].classList.contains("opened")) {
--- unicolor/script.js
+++ unicolor/script.js
@@ -1,13 +1,4 @@
-var socksImages = [
-    "../img/sockYellow.png",
-    "../img/sockRed.png",
-    "../img/sockCyan.png",
-    "../img/sockPink.png",
-    "../img/sockBlue.png"
-];
-
-var currentSelected = 0;
-
+// List of facts to choose from when showing the under construction popup
 const FEITJES = [
     "Sunny Socks begonnen is al grap onder een paar vrienden: “sokken met witte strepen zouden niet verkopen” maar na ruim 23 jaar verkopen we ze nog steeds",
     "Materialen 100% natuurlijk en eerlijk zijn",
@@ -19,24 +10,102 @@ const FEITJES = [
     "Een archeoloog voor de grap een paar Sunny Socks in de sarcofaag van een farao heeft gelegd bij de opgraving"
 ];
 
+// Positions for the socks in the carousel
+var sockValues = [
+    {
+        width: "140px",
+        top: "10px",
+        left: "175px",
+        zindex: "0"
+    },
+    {
+        width: "160px",
+        top: "15px",
+        left: "260px",
+        zindex: "1"
+    },
+    {
+        width: "180px",
+        top: "20px",
+        left: "360px",
+        zindex: "2"
+    },
+    {
+        width: "160px",
+        top: "15px",
+        left: "480px",
+        zindex: "1"
+    },
+    {
+        width: "140px",
+        top: "10px",
+        left: "585px",
+        zindex: "0"
+    }
+];
+
+// The same positions but for mobile
+var mobileSockValues = [
+    {
+        width: "45px",
+        top: "6px",
+        left: "60px",
+        zindex: "0"
+    },
+    {
+        width: "52.5px",
+        top: "7.5px",
+        left: "87.5px",
+        zindex: "1"
+    },
+    {
+        width: "60px",
+        top: "10px",
+        left: "120px",
+        zindex: "2"
+    },
+    {
+        width: "52.5px",
+        top: "7.5px",
+        left: "160px",
+        zindex: "1"
+    },
+    {
+        width: "45px",
+        top: "6px",
+        left: "195px",
+        zindex: "0"
+    }
+];
+
 window.onload = () => {
-    refreshSocks();
-    navdropdown = document.getElementsByClassName("navDropdown")[0]
-    headerdropdown = document.getElementsByClassName("headerDropdown")[0]
-    sokkenImgs = document.getElementsByClassName("socksBestelwielImg")
+    // Change the socks sizes when the screen switches to mobile
+    addEventListener("resize", () => {
+        var values = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth) < 576 ? mobileSockValues : sockValues;
+        updateSocks(values);
+    })
+    // Push back the first sock to the end of the array and update it
+    document.getElementsByClassName("leftScroll")[0].addEventListener("click", () => {
+        var values = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth) < 576 ? mobileSockValues : sockValues;
+        values.unshift(values.pop());
+        updateSocks(values);
+    });
+    // Push the last sock in the array to the front and update it
+    document.getElementsByClassName("rightScroll")[0].addEventListener("click", () => {
+        var values = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth) < 576 ? mobileSockValues : sockValues;
+        values.push(values.shift());
+        updateSocks(values);
+    });
+    // The same dropdown code as in the main script because you cannot have 2 onloads
+    navdropdown = document.getElementsByClassName("navDropdown")[0];
+    headerdropdown = document.getElementsByClassName("headerDropdown")[0];
     navdropdown.addEventListener("click", () => {
         if (navdropdown.classList.contains("toggled")) {
             navdropdown.classList.remove("toggled");
-            headerdropdown.classList.remove("headerOpen")
-            Array.prototype.forEach.call(sokkenImgs, (element) => {
-              element.style.marginTop = `0px`;
-            })
+            headerdropdown.classList.remove("headerOpen");
         } else {
             navdropdown.classList.add("toggled");
             headerdropdown.classList.add("headerOpen");
-            Array.prototype.forEach.call(sokkenImgs, (element) => {
-              element.style.marginTop = `263.2px`;
-            })
         }
     })
     footerdropdowns = document.getElementsByClassName("footerItem");
@@ -52,92 +121,65 @@ window.onload = () => {
     })
 }
 
-function selectForward()
-{
-    currentSelected = (currentSelected+1)%5;
-    refreshSocks(-1);
+// Update the size and position for every sock in the carousel
+function updateSocks(values) {
+    var images = document.getElementsByClassName("carouselImage");
+    Array.prototype.forEach.call(images, (element, index) => {
+        element.style.width = values[index].width;
+        element.style.top = values[index].top;
+        element.style.left = values[index].left;
+        element.style.zIndex = values[index].zindex;
+    });
 }
 
-function selectBackward()
-{
-    currentSelected = currentSelected-1;
-    if (currentSelected < 0) { currentSelected = 4; }
-    refreshSocks(1);
-}
-
-function refreshSocks(mod = 0)
-{
-    Array.prototype.forEach.call(document.getElementsByClassName("socksBestelwielImg"), (element, index) => {
-        let pos = index + mod;
-        let leftMargin = (navigator.userAgentData.mobile ? -15 : 0);
-        let bottomMargin = (navigator.userAgentData.mobile ? 50 : 0);
-        if (pos < 0) { pos = 4; }
-        if (pos > 4) { pos = 0; }
-        if (mod != 0) { element.style.transition = "all 0.2s linear"; }
-        else { element.style.transition = "none"; }
-        element.style.left = `${leftMargin+45+(pos-2)*5.5}%`;
-        element.style.bottom = `${bottomMargin+(40+Math.abs(pos-2)*2)}%`;
-        element.style.width = `${180+Math.abs(pos-2)*-20}px`;
-        element.style.zIndex = `${3+Math.abs(pos-2)*-1}`;
-        if (mod == 0) { element.src = socksImages[(pos+currentSelected)%5]; }
-    })
-
-    if (mod != 0) {
-        window.setTimeout(function () {
-            refreshSocks();
-        }, 200);
-    }
-}
-
-function popup()
-{
-    Array.prototype.forEach.call(document.getElementsByClassName("darken"), (darken, index) => {
+// Open the under construction popup
+function popup() {
+    Array.prototype.forEach.call(document.getElementsByClassName("darken"), (darken) => {
         darken.style.visibility = "visible";
         darken.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-    })
-    Array.prototype.forEach.call(document.getElementsByClassName("popup"), (popup, index) => {
+    });
+    Array.prototype.forEach.call(document.getElementsByClassName("popup"), (popup) => {
         popup.style.marginTop = "0px";
         popup.style.visibility = "visible";
-    })
+    });
     document.getElementById("weetje").innerHTML = `${getWeetje()}`;
 }
 
-function closePopup()
-{
-    Array.prototype.forEach.call(document.getElementsByClassName("popup"), (popup, index) => {
+// Close the under construction popup
+function closePopup() {
+    Array.prototype.forEach.call(document.getElementsByClassName("popup"), (popup) => {
         popup.style.marginTop = "200px";
         popup.style.visibility = "hidden";
-    })
-    Array.prototype.forEach.call(document.getElementsByClassName("darken"), (darken, index) => {
+    });
+    Array.prototype.forEach.call(document.getElementsByClassName("darken"), (darken) => {
         darken.style.backgroundColor = "rgba(0, 0, 0, 0)";
-        window.setTimeout(function () {
+        window.setTimeout(() => {
             darken.style.visibility = "hidden";
         }, 200);
-    })
+    });
 }
 
-function getWeetje()
-{
+// Check if a randomly selected fact is also the last one and return a new one nonetheless
+function getWeetje() {
     let lastWeetje = getCookie("last_weetje");
     let weetjeIndex = Math.floor(Math.random() * FEITJES.length);
-    if (lastWeetje == "")
-    {
-        document.cookie = `last_weetje=${weetjeIndex}`
+    if (lastWeetje === "") {
+        document.cookie = `last_weetje=${weetjeIndex}`;
         return FEITJES[weetjeIndex];
     }
     lastWeetje = parseInt(lastWeetje);
     for (let i = 0; i < FEITJES.length; i++) {
         weetjeIndex = Math.floor(Math.random() * FEITJES.length);
-        if (weetjeIndex != lastWeetje)
-        {
-            document.cookie = `last_weetje=${weetjeIndex}`
+        if (weetjeIndex != lastWeetje) {
+            document.cookie = `last_weetje=${weetjeIndex}`;
             return FEITJES[weetjeIndex];
         }
     }
-    document.cookie = `last_weetje=${weetjeIndex}`
+    document.cookie = `last_weetje=${weetjeIndex}`;
     return FEITJES[weetjeIndex];
 }
 
+// Return the value of a cookie from a specified key
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
